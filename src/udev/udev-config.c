systemd/systemd

udev: also reload udev.conf when explicitly requested
When reloading is explicitly requested, e.g. by 'udevadm control --reload',
then also reload udev.conf.

src/udev/udev-config.c
@@ -339,3 +339,26 @@ int manager_load(Manager *manager, int argc, char *argv[]) {
         manager_adjust_config(&manager->config);
         return 1;
 }
+
+UdevReloadFlag manager_reload_config(Manager *manager) {
+        assert(manager);
+
+        UdevConfig old = manager->config;
+
+        manager_parse_udev_config(&manager->config_by_udev_conf);
+        manager_merge_config(manager);
+        log_set_max_level(manager->config.log_level);
+        manager_adjust_config(&manager->config);
+
+        if (manager->config.resolve_name_timing != old.resolve_name_timing)
+                return UDEV_RELOAD_RULES | UDEV_RELOAD_KILL_WORKERS;
+
+        if (manager->config.log_level != old.log_level ||
+            manager->config.exec_delay_usec != old.exec_delay_usec ||
+            manager->config.timeout_usec != old.timeout_usec ||
+            manager->config.timeout_signal != old.timeout_signal ||
+            manager->config.blockdev_read_only != old.blockdev_read_only)
+                return UDEV_RELOAD_KILL_WORKERS;
+
+        return 0;
+}

src/udev/udev-config.h
@@ -27,4 +27,5 @@ typedef struct UdevConfig {
         }
 
 int manager_load(Manager *manager, int argc, char *argv[]);
+UdevReloadFlag manager_reload_config(Manager *manager);
 void udev_config_set_default_children_max(UdevConfig *c);

redhat-plumbers/systemd-rhel10

(cherry picked from commit e95861d909e17d56eddc20331a62f67f0d37d950)
Resolves: RHEL-75774

src/udev/udev-config.c
@@ -339,3 +339,27 @@ int manager_load(Manager *manager, int argc, char *argv[]) {
         manager_adjust_config(&manager->config);
         return 1;
 }
+
+UdevReloadFlags manager_reload_config(Manager *manager) {
+        assert(manager);
+
+        UdevConfig old = manager->config;
+
+        manager->config_by_udev_conf = UDEV_CONFIG_INIT;
+        manager_parse_udev_config(&manager->config_by_udev_conf);
+        manager_merge_config(manager);
+        log_set_max_level(manager->config.log_level);
+        manager_adjust_config(&manager->config);
+
+        if (manager->config.resolve_name_timing != old.resolve_name_timing)
+                return UDEV_RELOAD_RULES | UDEV_RELOAD_KILL_WORKERS;
+
+        if (manager->config.log_level != old.log_level ||
+            manager->config.exec_delay_usec != old.exec_delay_usec ||
+            manager->config.timeout_usec != old.timeout_usec ||
+            manager->config.timeout_signal != old.timeout_signal ||
+            manager->config.blockdev_read_only != old.blockdev_read_only)
+                return UDEV_RELOAD_KILL_WORKERS;
+
+        return 0;
+}

src/udev/udev-config.h
@@ -27,4 +27,5 @@ typedef struct UdevConfig {
         }
 
 int manager_load(Manager *manager, int argc, char *argv[]);
+UdevReloadFlags manager_reload_config(Manager *manager);
 void udev_config_set_default_children_max(UdevConfig *c);